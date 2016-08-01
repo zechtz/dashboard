aac88c8d06baeb377039db818a6ddcf76f3f893d
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-01 13:11:37 +0300

    Add dashboard route

--- config/routes/home.js
+++ config/routes/home.js
@@ -1,6 +1,15 @@
 var router = require('express').Router();
 
 /* GET index page  */
-router.get('/', function(req, res, next){
-  res.render('index');
+router.get('/', function(req, res){
+  res.render('index', {
+      title: 'Dashboard'
+  });
 });
+
+router.get('/dashboard', function(req, res){
+  res.render('home', {
+      title: 'Home'
+  });
+});
+
